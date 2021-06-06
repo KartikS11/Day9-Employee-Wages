4b0524f32471f1ce16de7bb51dbf0f64a7ce44b1
KartikS11 <kartiksonawane1011@gmail.com>
2021-06-07 00:08:16 +0530

    first commit

--- empwages.sh
+++ empwages.sh
@@ -2,3 +2,10 @@
 
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 
+isPresent=$(( RANDOM%2 ))
+
+if [[ $isPresent -eq 1 ]]; then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
