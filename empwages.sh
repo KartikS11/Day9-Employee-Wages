4baf6a326882b4ba8dbd40f60d33962adb19ebb7
KartikS11 <kartiksonawane1011@gmail.com>
2021-06-07 00:42:53 +0530

    seventh commit

--- empwages.sh
+++ empwages.sh
@@ -13,21 +13,34 @@ isFull=2
 day=1
 totalHrs=0
 
+function wageHrs(){
+local hrs=0
+if [[ $attendence -eq 0 ]]; then
+	hrs=$(( $1+0 ))
+        echo $hrs
+elif [[ $attendence -eq 1 ]]; then
+	hrs=$(( $1+$partHr ))
+	echo $hrs
+elif [[ $attendence -eq 2 ]]; then
+	hrs=$(( $1+$fullHr ))
+	echo $hrs
+fi
+}
+
 while [[ $day -le 20 ]]; do
 	attendence=$((RANDOM%3))
 
 case $attendence in
 	0)wage=$(($wage+0))
-	  totalHrs=$(( $totalHrs+0 ))
 	  ;;
 	1)wage=$(( $wage+ ($wagePerHr*$partHr) ))
-	  totalHrs=$(( $totalHrs+$partHr ))
 	  ;;
 	2)wage=$(( $wage+ ($wagePerHr*$fullHr) ))
-	  totalHrs=$(( $totalHrs+$fullHr ))
 	  ;;
 	3)echo "Attendence Error"
 esac
+
+	totalHrs=`wageHrs $totalHrs`
 
 if [[ $totalHrs -eq 100 ]]; then
 break
@@ -38,3 +51,4 @@ done
 echo "Days = $day"
 echo "Hours = $totalHrs"
 echo "Wage for a month(i.e. 20 days or 100 hrs) = $wage"
+
