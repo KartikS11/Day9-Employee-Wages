15a2d15fd5d5d7423c20a1449872db01ea807f53
KartikS11 <kartiksonawane1011@gmail.com>
2021-06-07 00:17:30 +0530

    third commit

--- empwages.sh
+++ empwages.sh
@@ -5,6 +5,8 @@ echo "Welcome to Employee Wage Computation Program on Master Branch"
 isPresent=$(( RANDOM%2 ))
 wagePerHr=20
 fulldayHr=8
+partdayHr=4
+fullPart=$((RANDOM%2))
 
 if [[ $isPresent -eq 1 ]]; then
 	echo "Employee is Present"
@@ -12,4 +14,8 @@ else
 	echo "Employee is Absent"
 fi
 
-fulldayWage=$(($wagePerHr*fulldayHr))
+if [[ $fullPart -eq 1 ]]; then
+	fulldayWage=$(($wagePerHr*$fulldayHr))
+elif [[ $fullPart -eq 0 ]]; then
+	partdayWage=$(($wagePerHr*$partdayHr))
+fi
