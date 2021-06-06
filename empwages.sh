1b43e94bc119105ea3c0cc2ac20a018a82a78299
KartikS11 <kartiksonawane1011@gmail.com>
2021-06-07 00:48:15 +0530

    eighth commit

--- empwages.sh
+++ empwages.sh
@@ -2,22 +2,24 @@
 
 echo "Welcome to Employee Wage Computation Program on Master branch"
 
+arr=()
 wagePerHr=20
 fullHr=8
 partHr=4
 wage=0
+dailywage=0
 
 isAbsent=0
 isPart=1
 isFull=2
-day=1
+day=0
 totalHrs=0
 
 function wageHrs(){
 local hrs=0
 if [[ $attendence -eq 0 ]]; then
 	hrs=$(( $1+0 ))
-        echo $hrs
+	echo $hrs
 elif [[ $attendence -eq 1 ]]; then
 	hrs=$(( $1+$partHr ))
 	echo $hrs
@@ -27,19 +29,22 @@ elif [[ $attendence -eq 2 ]]; then
 fi
 }
 
-while [[ $day -le 20 ]]; do
+while [[ $day -lt 20 ]]; do
 	attendence=$((RANDOM%3))
 
 case $attendence in
 	0)wage=$(($wage+0))
+	  dailywage=0
 	  ;;
 	1)wage=$(( $wage+ ($wagePerHr*$partHr) ))
+	  dailywage=$(($wagePerHr*$partHr))
 	  ;;
 	2)wage=$(( $wage+ ($wagePerHr*$fullHr) ))
+	  dailywage=$(($wagePerHr*$fullHr))
 	  ;;
-	3)echo "Attendence Error"
+ 	3)echo "Attendence Error"
 esac
-
+	arr[$day]=$dailywage 
 	totalHrs=`wageHrs $totalHrs`
 
 if [[ $totalHrs -eq 100 ]]; then
@@ -50,5 +55,6 @@ done
 
 echo "Days = $day"
 echo "Hours = $totalHrs"
+echo "Daily wages for 1 Month are: "
+echo ${arr[@]}
 echo "Wage for a month(i.e. 20 days or 100 hrs) = $wage"
-
