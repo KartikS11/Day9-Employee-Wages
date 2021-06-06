92da678a471bbd9599c29c9223eeef7f2af1802c
KartikS11 <kartiksonawane1011@gmail.com>
2021-06-07 00:31:51 +0530

    sixth commit

--- empwages.sh
+++ empwages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Welcome to Employee Wage Computation Program on Master Branch"
+echo "Welcome to Employee Wage Computation Program on Master branch"
 
 wagePerHr=20
 fullHr=8
@@ -11,20 +11,30 @@ isAbsent=0
 isPart=1
 isFull=2
 day=1
+totalHrs=0
 
 while [[ $day -le 20 ]]; do
 	attendence=$((RANDOM%3))
-case $attendence in
 
+case $attendence in
 	0)wage=$(($wage+0))
+	  totalHrs=$(( $totalHrs+0 ))
 	  ;;
 	1)wage=$(( $wage+ ($wagePerHr*$partHr) ))
+	  totalHrs=$(( $totalHrs+$partHr ))
 	  ;;
 	2)wage=$(( $wage+ ($wagePerHr*$fullHr) ))
+	  totalHrs=$(( $totalHrs+$fullHr ))
 	  ;;
 	3)echo "Attendence Error"
 esac
+
+if [[ $totalHrs -eq 100 ]]; then
+break
+fi
 ((day++))
 done
 
-echo "Wage for a month(i.e. 20 days) = $wage"
+echo "Days = $day"
+echo "Hours = $totalHrs"
+echo "Wage for a month(i.e. 20 days or 100 hrs) = $wage"
